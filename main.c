034790af09c087bff8a706d46127b36f3826a370
Пидяков Дмитрий Евгеньевич <ip816s20@mwp06.csc.local>
2019-04-15 08:26:14 +0700

    Working  code

--- main.c
+++ main.c
@@ -31,12 +31,8 @@ int main()
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
 
     const int min = min_element(arr, N_ELEMENTS(arr));
-
-<<<<<<< HEAD
-=======
     print_array(arr, N_ELEMENTS(arr));
     printf("Min element: %d\n", min);
->>>>>>> origin/develop
 
     return 0;
 }
